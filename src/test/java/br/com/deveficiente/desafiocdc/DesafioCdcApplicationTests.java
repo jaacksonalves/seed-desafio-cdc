516226eb95956b665645c8576464cd448b012eb0
jaacksonalves <jacksonalves.pro@gmail.com>
2022-09-02 11:37:27 -0300

    feat: commit inicial

---
+++ src/test/java/br/com/deveficiente/desafiocdc/DesafioCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.desafiocdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

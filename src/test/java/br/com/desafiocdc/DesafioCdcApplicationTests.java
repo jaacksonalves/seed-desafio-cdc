61efbc6fd3199509c52ab0478998beb2d587fb76
jaacksonalves <jacksonalves.pro@gmail.com>
2025-12-02 11:05:45 -0300

    Adicionando meu projeto inicial

---
+++ src/test/java/br/com/desafiocdc/DesafioCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiocdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
